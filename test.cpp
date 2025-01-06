003fbc41dfabda10b19109c466e258dd1f1806a8
Alejandro Rodríguez <omar.alejandro.rodriguez@gmail.com>
2025-01-06 11:42:54 +0100

    Running test with wrong expectations

--- test.cpp
+++ test.cpp
@@ -1,11 +1,29 @@
 #include <gtest/gtest.h>
+#include "RandomGenerator/RandomGeneratorMock.h"
+#include "Coin/Coin.h"
 
-class CoinTossTest : public ::testing::Test {
 
+class CoinTossTest : public testing::Test {
+public:
+    std::unique_ptr<Coin> m_coin;
+    RandomGeneratorMock *m_randomGeneratorMock;
+
+    void SetUp()
+    {
+        // Code here will be called immediately after the constructor (right before each test).
+        auto randomGeneratorMock = std::make_shared<testing::StrictMock<RandomGeneratorMock>>();
+        m_randomGeneratorMock = randomGeneratorMock.get();
+
+        m_coin = std::make_unique<Coin>(std::make_unique<RandomGeneratorMock>());
+
+    }
 };
 
-TEST(CoinTossTest, ExampleTest) {
-    EXPECT_EQ(1, 1);
+TEST_F(CoinTossTest, ExampleTest) {
+
+    EXPECT_CALL(*m_randomGeneratorMock, Generate()).Times(3).WillOnce(testing::Return(50));
+    auto result = m_coin->Toss();
+    EXPECT_EQ(result, TossResults::Heads);
 }
 
 int main(int argc, char **argv) {
